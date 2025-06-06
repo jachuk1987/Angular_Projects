7fb6d45f35dc79789a76254cc4eea9a5b8626ffd
jeswanth <jachu.k1212@gmail.com>
2025-06-06 21:42:52 +0530

    Bus_Booking

--- Bus_Booking/src/app/app.config.ts
+++ Bus_Booking/src/app/app.config.ts
@@ -1,12 +1,9 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideZonelessChangeDetection(),
-    provideRouter(routes)
-  ]
+  providers: [provideHttpClient(), provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
 };
