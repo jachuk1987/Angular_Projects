e6aeb8e26011f1f8f5011e9c98ddc2dbc5a90248
jeswanth <jachu.k1212@gmail.com>
2025-06-11 12:05:17 +0530

    Bus_Booking

--- Bus_Booking/src/app/service/master.service.ts
+++ Bus_Booking/src/app/service/master.service.ts
@@ -1,3 +1,50 @@
+// import { HttpClient } from '@angular/common/http';
+// import { Injectable } from '@angular/core';
+// import { Observable } from 'rxjs';
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class MasterService {
+
+//   // apiURl: string = 'https://projectapi.gerasim.in/api/BusBooking/';
+//   apiURl: string = '/api/api/BusBooking/';
+
+
+//   constructor(private http: HttpClient) { }
+
+//   getLocations(): Observable<any[]> {
+//     return this.http.get<any[]>(this.apiURl + "GetBusLocations")
+//   }
+
+//   serachBus(from: number, to: number, travelDate: string): Observable<any[]> {
+//     return this.http.get<any[]>(`${this.apiURl}searchBus?fromLocation=${from}&toLocation=${to}&travelDate=${travelDate}`)
+//   }
+
+//   getScehduelById(id: number) {
+//     return this.http.get<any[]>(this.apiURl + "GetBusScheduleById?id="+id)
+    
+//   }
+
+//   getBookedSeats(id: number) {
+//     return this.http.get<any[]>(this.apiURl + "getBookedSeats?shceduleId="+id)
+    
+//   }
+
+//   onRegisterUser(obj: any) {
+//     return this.http.post<any[]>(this.apiURl + "AddNewUser", obj)
+    
+//   }
+  
+//   onBooking(obj: any) {
+//     return this.http.post<any[]>(this.apiURl + "PostBusBooking", obj)
+    
+//   }
+
+
+// }
+
+
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
@@ -7,38 +54,32 @@ import { Observable } from 'rxjs';
 })
 export class MasterService {
 
-  apiURl: string = 'https://projectapi.gerasim.in/api/BusBooking/';
-
+  // Use proxy URL for development
+  apiURl: string = '/api/api/BusBooking/';
 
   constructor(private http: HttpClient) { }
 
   getLocations(): Observable<any[]> {
-    return this.http.get<any[]>(this.apiURl + "GetBusLocations")
+    return this.http.get<any[]>(this.apiURl + "GetBusLocations");
   }
 
   serachBus(from: number, to: number, travelDate: string): Observable<any[]> {
-    return this.http.get<any[]>(`${this.apiURl}searchBus?fromLocation=${from}&toLocation=${to}&travelDate=${travelDate}`)
+    return this.http.get<any[]>(`${this.apiURl}searchBus?fromLocation=${from}&toLocation=${to}&travelDate=${travelDate}`);
   }
 
-  getScehduelById(id: number) {
-    return this.http.get<any[]>(this.apiURl + "GetBusScheduleById?id="+id)
-    
+  getScehduelById(id: number): Observable<any[]> {
+    return this.http.get<any[]>(this.apiURl + "GetBusScheduleById?id=" + id);
   }
 
-  getBookedSeats(id: number) {
-    return this.http.get<any[]>(this.apiURl + "getBookedSeats?shceduleId="+id)
-    
+  getBookedSeats(id: number): Observable<any[]> {
+    return this.http.get<any[]>(this.apiURl + "getBookedSeats?shceduleId=" + id);
   }
 
-  onRegisterUser(obj: any) {
-    return this.http.post<any[]>(this.apiURl + "AddNewUser", obj)
-    
-  }
-  
-  onBooking(obj: any) {
-    return this.http.post<any[]>(this.apiURl + "PostBusBooking", obj)
-    
+  onRegisterUser(obj: any): Observable<any[]> {
+    return this.http.post<any[]>(this.apiURl + "AddNewUser", obj);
   }
 
-
+  onBooking(obj: any): Observable<any[]> {
+    return this.http.post<any[]>(this.apiURl + "PostBusBooking", obj);
+  }
 }
