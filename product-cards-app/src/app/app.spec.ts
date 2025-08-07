25db46081f855625799b1e5ff47ea90adac2bb20
jeswanth <jachu.k1212@gmail.com>
2025-08-07 12:50:06 +0530

    Angular_Projects

--- product-cards-app/src/app/app.spec.ts
+++ product-cards-app/src/app/app.spec.ts
@@ -20,6 +20,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, products');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, product-cards-app');
   });
 });
--- product-cards-app/src/app/app.ts
+++ product-cards-app/src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected title = 'products';
+  protected title = 'product-cards-app';
 }
