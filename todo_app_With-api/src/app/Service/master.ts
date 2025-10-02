0243651b30146b5b467ac03eb5fb904066f9256d
jeswanth <jachu.k1212@gmail.com>
2025-10-02 08:38:35 +0530

    Angular_Todo_App

--- todo_app_With-api/src/app/Service/master.ts
+++ todo_app_With-api/src/app/Service/master.ts
@@ -1,8 +1,11 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class Master {
+  
+  constructor(private http: HttpClient) {}
   
 }
