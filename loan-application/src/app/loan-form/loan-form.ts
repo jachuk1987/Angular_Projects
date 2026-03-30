9c06420dc72e1634732e939dd40e9ebacb44a8b1
jeswanth <jachu.k1212@gmail.com>
2026-03-30 15:18:31 +0530

    AngularProject

---
+++ loan-application/src/app/loan-form/loan-form.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loan-form',
+  imports: [],
+  templateUrl: './loan-form.html',
+  styleUrl: './loan-form.css',
+})
+export class LoanForm {
+
+}
