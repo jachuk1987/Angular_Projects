8349aa67b9a71fbedbb05c1e6fd2dfae9d588fcc
jeswanth <jachu.k1212@gmail.com>
2026-03-30 12:12:22 +0530

    AngularProject

---
+++ loan-application/src/app/features/loan/components/loan-details/loan-details.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loan-details',
+  imports: [],
+  templateUrl: './loan-details.html',
+  styleUrl: './loan-details.css',
+})
+export class LoanDetails {
+
+}
